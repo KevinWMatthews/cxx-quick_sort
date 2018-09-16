595696f8fde7d5629ab316b06c09029c3b02e798
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-16 15:32:24 -0700

    Add test with single element

--- tests/test_quick_sort.cpp
+++ tests/test_quick_sort.cpp
@@ -17,3 +17,18 @@ TEST_F(QuickSortZeroElementsTest, DoNotCrashWithNoElements)
     quick_sort.sort();
     ASSERT_EQ(expected, actual);
 }
+
+class QuickSortOneElementTest : public ::testing::Test
+{
+protected:
+    using data_type = array<int, 1>;
+};
+
+TEST_F(QuickSortOneElementTest, SingleElementIsAlreadySorted)
+{
+    data_type expected = {10};
+    data_type actual   = {10};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
