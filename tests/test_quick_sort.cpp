6f71b4f682f6d0b6fe8bfa5c9a876f1fd515cdd6
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-17 08:50:29 -0700

    Add failing tests for sorting three elements
    Write down all permutations that I can think of
    before trying to implement this.

--- tests/test_quick_sort.cpp
+++ tests/test_quick_sort.cpp
@@ -65,3 +65,91 @@ TEST_F(QuickSortTwoElementsTest, UnsortedElementsGetSorted)
     quick_sort.sort();
     ASSERT_EQ(expected, actual);
 }
+
+
+class QuickSortThreeElementsTest : public ::testing::Test
+{
+protected:
+    using data_type = array<int, 3>;
+};
+
+TEST_F(QuickSortThreeElementsTest, IdentialElementsAreAlreadySorted)
+{
+    data_type expected = {10, 10, 10};
+    data_type actual   = {10, 10, 10};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, RemainSortedIfFirstPairIdentical)
+{
+    data_type expected = {10, 10, 11};
+    data_type actual   = {10, 10, 11};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, RemainSortedIfSecondPairIdentical)
+{
+    data_type expected = {10, 12, 12};
+    data_type actual   = {10, 12, 12};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, UniqueElementsRemainSorted)
+{
+    data_type expected = {10, 11, 12};
+    data_type actual   = {10, 11, 12};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, SortsFirstPairOfElements)
+{
+    data_type expected = {10, 11, 12};
+    data_type actual   = {11, 10, 12};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, SortsSecondPairOfElements)
+{
+    data_type expected = {10, 11, 12};
+    data_type actual   = {10, 12, 11};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, MiddleElementMovesToBeginning)
+{
+    data_type expected = {10, 11, 11};
+    data_type actual   = {11, 10, 11};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, MiddleElementMovesToEnd)
+{
+    data_type expected = {10, 10, 12};
+    data_type actual   = {10, 12, 10};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(QuickSortThreeElementsTest, WorstCaseGetsSorted)
+{
+    data_type expected = {10, 11, 12};
+    data_type actual   = {12, 11, 10};
+    QuickSort<data_type> quick_sort = {actual};
+    quick_sort.sort();
+    ASSERT_EQ(expected, actual);
+}
