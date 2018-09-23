dbf9e3ffb72cba16643032ee85e87e110a3a1c8c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-23 12:17:42 -0700

    Rename internal function

--- include/quick_sort.h
+++ include/quick_sort.h
@@ -17,14 +17,14 @@ public:
 
         auto begin = data_.begin();
         auto end = data_.end() - 1;     // Stop while pointing to valid data
-        quick_sort_partition(begin, end);
+        quick_sort(begin, end);
     };
 private:
     using iter_type = typename T::iterator;
 
     // Partition and sort the given range, following the quick sort algorithm
     // Recursive.
-    void quick_sort_partition(iter_type begin, iter_type end) {
+    void quick_sort(iter_type begin, iter_type end) {
         if (std::distance(begin, end) < 1)
             return;
 
@@ -62,8 +62,8 @@ private:
         // Sort before and after the partition element's new position.
         // The partition element is already in the correct location,
         // so don't include it in the sort.
-        quick_sort_partition(begin, new_pkey-1);
-        quick_sort_partition(new_pkey+1, end);
+        quick_sort(begin, new_pkey-1);
+        quick_sort(new_pkey+1, end);
     };
     T& data_;
 };
